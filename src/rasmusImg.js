cfdf64ec53a3008bbc95df947d2bb0c4c7856756
rasm2 <rasm202m@gmail.com>
2026-02-19 11:21:32 +0100

    Added a new Yes image, and change it to be the shown image

--- src/rasmusImg.js
+++ src/rasmusImg.js
@@ -1,9 +1,10 @@
-import IMG from './rasmusImg.jpg';
-import IMG2 from './rasmusSadImg.jpg';
+import YESIMG1 from './rasmusYes1.jpg';
+import YESIMG2 from './rasmusYes2.jpg';
+import NOIMG1 from './rasmusNo1.jpg';
 import './rasmusImg.css';
 
 function RasmusImg() {
-  return <img src={IMG2} alt="Rasmus" />;
+  return <img src={YESIMG2} alt="Rasmus" />;
 }
 
 export default RasmusImg;
