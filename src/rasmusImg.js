394981387b96ad829d464c82a4b799e6ddc14f97
rasm2 <rasm202m@gmail.com>
2026-04-02 21:28:53 +0200

    Feature: Added new Yes image.

--- src/rasmusImg.js
+++ src/rasmusImg.js
@@ -1,10 +1,11 @@
 import YESIMG1 from './rasmusYes1.jpg';
 import YESIMG2 from './rasmusYes2.jpg';
+import YESIMG3 from './rasmusYes3.jpg';
 import NOIMG1 from './rasmusNo1.jpg';
 import './rasmusImg.css';
 
 function RasmusImg() {
-  return <img src={YESIMG2} alt="Rasmus" />;
+  return <img src={YESIMG3} alt="Rasmus" />;
 }
 
 export default RasmusImg;
